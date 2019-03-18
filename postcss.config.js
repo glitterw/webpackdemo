d6eedf3c51f58ddbb2ddf9ed4f91b35fd3b4a547
glitterw <glitter333@163.com>
2019-03-18 13:48:51 +0800

    配置css前缀， 修改postcss.config.js文件中浏览器的配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+// 必须设置浏览器类型才会自动添加前缀
 module.exports = {
-    plugins: [require('autoprefixer')]  // 引用该插件即可了
+    plugins:[
+        require('autoprefixer')({
+            "browsers": [
+                "defaults",
+                "not ie < 11",
+                "last 2 versions",
+                "> 1%",
+                "iOS 7",
+                "last 3 iOS versions"
+            ]
+        })
+    ]
 }
